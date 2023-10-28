87c5e72ad544579255f94045eb548b9fbf966e4e
Nicolás Olmos <yohannicolas2003@gmail.com>
2023-10-27 23:24:32 -0500

    feat: db connection to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './configs/dbConfig';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [TypeOrmModule.forRoot(getDatabaseConfig()), AuthModule, UserModule],
   controllers: [AppController],
   providers: [],
 })
