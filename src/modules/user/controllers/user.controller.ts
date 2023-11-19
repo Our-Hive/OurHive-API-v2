a541c821758468e3a36ac8cf0d6a11581db5b45a
Nicolás Olmos <yohannicolas2003@gmail.com>
2023-11-19 01:47:13 -0500

    feat: user controllers use token

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserService } from '../services/user.service';
-import { DeactiveAccountRequestDto } from 'src/modules/auth/dtos/deactivate.request.dto';
 import { ApiBody } from '@nestjs/swagger';
-
+import { DeactiveAccountRequestDto } from 'src/modules/auth/dtos/deactivate.request.dto';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt.guard';
+import { Request } from 'express';
+import { PayloadTokenDto } from 'src/modules/auth/models/token.model';
+@UseGuards(JwtAuthGuard)
 @ApiTags('User')
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -12,19 +22,21 @@ export class UserController {
   @ApiOperation({ summary: 'Get user by ID' })
   @ApiResponse({ status: 200, description: 'User found' })
   @ApiResponse({ status: 404, description: 'User not found' })
-  @Get(':id')
-  getById(@Param('id') id: number) {
-    return this.userService.findById(id);
+  @ApiHeader({ name: 'Authorization', description: 'Auth token' })
+  @Get()
+  getById(@Req() req: Request) {
+    const loggedUser = req.user as PayloadTokenDto;
+    return this.userService.findById(loggedUser.sub);
   }
 
-  // ...
-
-  @ApiOperation({ summary: 'Deactivate user by ID' })
+  @ApiOperation({ summary: 'Deactivate user' })
   @ApiResponse({ status: 200, description: 'User deactivated' })
   @ApiResponse({ status: 404, description: 'User not found' })
-  @Post(':id/deactivate')
+  @Post('deactivate')
+  @ApiHeader({ name: 'Authorization', description: 'Auth token' })
   @ApiBody({ type: DeactiveAccountRequestDto })
-  deactivate(@Param('id') id: number, @Body() user: DeactiveAccountRequestDto) {
-    return this.userService.deactivate(id, user);
+  deactivate(@Req() req: Request, @Body() user: DeactiveAccountRequestDto) {
+    const loggedUser = req.user as PayloadTokenDto;
+    return this.userService.deactivate(loggedUser.sub, user);
   }
 }
