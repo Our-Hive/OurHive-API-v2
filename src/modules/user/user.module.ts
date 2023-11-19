83efb8f5fbe66e5bbe92efec8db99a11e50139d1
Nicolás Olmos <yohannicolas2003@gmail.com>
2023-11-18 20:52:28 -0500

    Update findById method return type

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserControllerTsController } from './controllers/user.controller';
 
 @Module({
   providers: [UserService],
   // TypOrmModule.forFeature([User]) will inject the User repository into the UserService.
   imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserControllerTsController],
 })
 export class UserModule {}
